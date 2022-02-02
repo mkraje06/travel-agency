1cbb7b084595f87f9a3b140c6fa635e8248e5ecf
Maksymilian Krajewski <maksymilian.krajewski@yahoo.com>
2022-02-02 13:57:04 +0100

    format of time works

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,22 @@
-export const formatTime = () => "formatted time";
+export const formatTime = (param) => {
+  if (typeof param == "undefined") {
+    return null;
+  } else if (typeof param !== "number") {
+    return null;
+  } else if (param < 0) {
+    return null;
+  } else {
+    const seconds = Math.floor(param % 60)
+      .toString()
+      .padStart(2, "0");
+    const minutes = Math.floor((param / 60) % 60)
+      .toString()
+      .padStart(2, "0");
+    const hours = Math.floor(param / 3600)
+      .toString()
+      .padStart(2, "0");
+    let formattedTime = `${hours}:${minutes}:${seconds}`;
+
+    return formattedTime;
+  }
+};
